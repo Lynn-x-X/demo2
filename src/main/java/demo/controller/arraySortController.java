526eac966c77daf703df2f1a34a5715cf4201365
Local_01 <18205955685@163.com>
2025-02-02 14:35:27 +0000

    1

--- src/main/java/demo/controller/arraySortController.java
+++ src/main/java/demo/controller/arraySortController.java
@@ -12,23 +12,29 @@ import java.util.List;
 public class arraySortController {
     @PostMapping("/sort")
     public ResponseEntity<?> sortArray(@RequestBody RequestData requestData) {
+        System.out.println(requestData);
+
         List<Integer> array=requestData.getArray();
 
         if(array==null){
+            System.out.println("array is null");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(Collections.singletonMap("error","Array cannot be null"));
         }
         if( array.size()>1000){
+            System.out.println("array is too long");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(Collections.singletonMap("error","Array cannot be longer than 1000"));
         }
         for(Object element:array){
             if(!(element instanceof Integer)){
+                System.out.println("element is not an integer");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                         .body(Collections.singletonMap("error","Array must be an integer"));
             }
         }
 
+        System.out.println(array);
         Collections.sort(array);
         return ResponseEntity.ok(Collections.singletonMap("SortedArray",array));
 
