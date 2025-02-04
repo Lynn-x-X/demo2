001f8964b9e0c5af76f80a5ebbad0bd532378e44
Local_01 <18205955685@163.com>
2025-02-04 01:47:40 +0000

    1

--- src/main/java/demo/controller/arraySortController.java
+++ src/main/java/demo/controller/arraySortController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class arraySortController {
     public ResponseEntity<?> sortArray(@RequestBody RequestData requestData) {
         System.out.println("ori"+requestData);
 
-        List<Integer> array=requestData.getArray();
+        List<Object> array=requestData.getArray();
 
         if(array==null){
             System.out.println("array is null");
@@ -26,16 +27,21 @@ public class arraySortController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(Collections.singletonMap("error","Array cannot be longer than 1000"));
         }
+
+        List<Integer> intList = new ArrayList<>();
         for(Object element:array){
             if(!(element instanceof Integer)){
                 System.out.println("element is not an integer");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                         .body(Collections.singletonMap("error","Array must be an integer"));
+            }else {
+                intList.add((Integer)element);
             }
         }
 
+
         System.out.println("arr"+array);
-        Collections.sort(array);
+        Collections.sort(intList);
         return ResponseEntity.ok(Collections.singletonMap("SortedArray",array));
 
     }
@@ -44,11 +50,11 @@ public class arraySortController {
 
 
     public static class RequestData {
-        private List<Integer> array;
-        public List<Integer> getArray() {
+        private List<Object> array;
+        public List<Object> getArray() {
             return array;
         }
-        public void setArray(List<Integer> array) {
+        public void setArray(List<Object> array) {
             this.array = array;
         }
     }
